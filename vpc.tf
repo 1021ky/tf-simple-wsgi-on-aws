e8ba06890d315df858a4d23ecc85e6cca047a445
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2023-03-09 07:54:48 +0900

    ネットワークを写経しつつ追加

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,26 @@
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.10.0.0/16"
 }
+data "aws_availability_zone" "az-us-west-2a" {
+  name = "us-west-2a"
+}
+data "aws_availability_zone" "az-us-west-2c" {
+  name = "us-west-2c"
+}
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.10.0.0/24"
+  availability_zone = "us-west-2a"
 
-resource "aws_subnet" "" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.10.0.0/24"
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.10.64.0/24"
+  availability_zone = "us-west-2a"
 
   tags = {
     Name = "Main"
@@ -17,4 +33,15 @@ resource "aws_internet_gateway" "gw" {
   tags = {
     Name = "main"
   }
+}
+
+resource "aws_eip" "nat_ip" {
+}
+
+resource "aws_nat_gateway" "ngw-from-private" {
+  subnet_id         = aws_subnet.public_subnet.id
+  connectivity_type = "public"
+  allocation_id     = aws_eip.nat_ip.id
+
+  depends_on = [aws_internet_gateway.gw]
 }
