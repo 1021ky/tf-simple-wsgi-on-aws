a0c9301c3abfbb0c44ae06c5146279658d425db4
Keisuke Yamanaka <2500024+1021ky@users.noreply.github.com>
2023-04-11 13:28:19 +0900

    ユーザーデータが正常に動くように修正

--- server_ec2.tf
+++ server_ec2.tf
@@ -9,24 +9,27 @@ resource "aws_instance" "web" {
   key_name        = aws_key_pair.basation_key_pair.id
   security_groups = [aws_security_group.ssh_sg.id, aws_security_group.web_sg.id]
   user_data       = <<-EOF
-  #!/bin/bash
-  sudo apt update -y
-  sudo apt install -y nginx
-  sudo sh -c 'cat << EOS > /etc/nginx/sites-available/default
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y nginx
+sudo bash -c 'cat << EOS > /etc/nginx/sites-available/default
 server {
     listen 8080;
     location /app {
         # APPリクエストをAPPサーバーへリダイレクト
-        proxy_pass http://10.10.64.159:3031;
+        proxy_pass http://${aws_instance.app.private_ip}:3031;
     }
 }
+EOS
 '
-  sudo ufw allow 'Nginx HTTP'
-  sudo systemctl reload nginx
-  EOF
+sudo systemctl reload nginx
+EOF
   tags = {
     Name = "webserver"
   }
+  depends_on = [
+    aws_instance.app
+  ]
 }
 
 resource "aws_instance" "app" {
@@ -37,19 +40,21 @@ resource "aws_instance" "app" {
   key_name        = aws_key_pair.basation_key_pair.id
   security_groups = [aws_security_group.ssh_sg.id, aws_security_group.app_sg.id]
   user_data       = <<-EOF
-  #!/bin/bash
-  sudo apt update -y
-  sudo apt install -y python3-pip python3-venv build-essential python-dev
-  sudo apt install uwsgi-core
-  pip3 install uwsgi
-  sudo mkdir -p /var/www/uwsgi
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y python3-pip python3-venv build-essential python-dev
+pip3 install uwsgi
+sudo mkdir -p /var/www/uwsgi
   sudo sh -c 'cat << EOS > /var/www/uwsgi/index.py
 def application(env, start_response):
     start_response("200 OK", [("Content-Type","text/html")])
     return [b"Hello World"]
+EOS
 '
-  uwsgi --http-socket :3031 --wsgi-file /var/www/uwsgi/index.py &
-  EOF
+# sudo chmod 777 /home/ubuntu/.local/bin/uwsgi
+# sudo chmod 777 /var/www/uwsgi/index/py
+uwsgi --http-socket :3031 --wsgi-file /var/www/uwsgi/index.py
+EOF
 
   tags = {
     Name = "appserver"
